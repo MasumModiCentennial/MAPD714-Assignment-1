c77657173f0215bb1d2c497858f46108659aeb3f
masummodi <masum.modi@yobored.com>
2020-09-30 02:58:51 -0400

    Initial Commit

---
+++ Assignment 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment 1
+//
+//  Created by Masum Modi on 2020-09-30.
+//  Copyright © 2020 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
