b46ab3da5c73eb2b3c066691eb5bbf26c443af90
masummodi <masum.modi@yobored.com>
2020-10-02 13:00:45 -0400

    Comments in ViewControler.swift file

--- Assignment 1/ViewController.swift
+++ Assignment 1/ViewController.swift
@@ -1,10 +1,10 @@
 //
 //  ViewController.swift
-//  Assignment 1
+//  Assignment 1:  Calculator App - Part 1 - Create the App UI
 //
 //  Created by Masum Modi on 2020-09-30.
 //  Copyright © 2020 Centennial College. All rights reserved.
-//
+//  Student Id: 301149321
 
 import UIKit
 
